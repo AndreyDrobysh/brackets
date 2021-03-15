bffc4277d70cd2e3fd81aa225968f785319d7366
Andrei Drobysh <andrey.drobysh.04@gmail.com>
2021-03-16 01:41:55 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = [];
+let close = [];
+let stack = [];
+for (let i = 0; i < bracketsConfig.length; i++) {
+  open.push(bracketsConfig[i][0]);
+  close.push(bracketsConfig[i][1]);
+}
+for (let i = 0; i < str.length; i++) {
+  if (open.includes(str[i])) {
+    stack.push(str[i]);
+    continue
+  }
+  let bracketIndex = close.indexOf(str[i])
+  let openBracket = open[bracketIndex]
+  let lastOpenBracket = stack.pop()
+  if (lastOpenBracket !== openBracket) {
+    return false
+  }
+}
+if (stack.length === 0) {
+  return true
+}
+return false;
 }
